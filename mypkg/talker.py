faf00c098ae109618ddb38373203fab76665d69b
Yuki Nishijima <s23C1101EC@s.chibakoudai.jp>
2025-01-04 12:04:31 +0900

    mypkg

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,22 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from std_msgs.msg import Int16  
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Person, "person", 10)
-n = 0
-
-
-def cb():
-    global n
-    msg = Person()
-    msg.name = "西島侑希"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
+class Talker(Node):
+    def __init__(self):
+        super().__init__("talker")
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.create_timer(0.5, self.cd)
+        self.n = 0
+    def cd(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
 
 def main():
-    node.create_timer(0.5, cb)
+    rclpy.init()
+    node = Talker()
     rclpy.spin(node)
