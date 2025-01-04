dd5c9064fab7d81b738f82c2e8759d31da4fca4c
Yuki Nishijima <s23C1101EC@s.chibakoudai.jp>
2025-01-04 12:21:58 +0900

    .

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -3,9 +3,7 @@ import launch.actions
 import launch.substitutions
 import launch_ros.actions
 
-
 def generate_launch_description():
-
     talker = launch_ros.actions.Node(
         package='mypkg',
         executable='talker',
