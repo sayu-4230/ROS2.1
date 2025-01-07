bf582faa36dac360dc4f517201716d746f779305
Yuki Nishijima <s23C1101EC@s.chibakoudai.jp>
2025-01-08 01:57:23 +0900

    .

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,20 +1,22 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query
+from std_ msgs.msg import Int16
 
-rclpy.init()
-node = Node("talker")
+class Talker(Node):
+    def __init__(self):
+        super().__init__("taker")
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.create_timer(0.5, self.cd)
+        self.n = 0
 
 
-def cb(request, response):
-    if request.name == "上田隆一":
-        response.age == 46
-    else:
-        response.age == 255
-
-    return response
-
+    def cd(self):
+        msg = Int16()
+        msg.date = self.n
+        self.pub.pub.publish(msg)
+        self.n += 1
 
 def main():
-    srv = node.create_service(Query, "query", cb)
+    rclpy.init()
+    node = Talker()
     rclpy.spin(node)
